5266a3cba8aa08319bbb0af2ace0829c0e34dd71
areibens <areibens@localhost>
2005-11-16 02:16:10 +0000

    revision workaround for SF cvs import not to screw up CPAN

--- lib/PDF/API2/Resource/CIDFont/TrueType.pm
+++ lib/PDF/API2/Resource/CIDFont/TrueType.pm
@@ -240,6 +240,9 @@ alfred reibenschuh
 =head1 HISTORY
 
     $Log$
+    Revision 2.0  2005/11/16 02:16:04  areibens
+    revision workaround for SF cvs import not to screw up CPAN
+
     Revision 1.2  2005/11/16 01:27:48  areibens
     genesis2
 
