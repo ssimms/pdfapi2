7e7657d9993c75991a8b2417f88af6afca62bdf6
areibens <areibens@cpan.org>
2005-11-16 02:16:10 +0000

    revision workaround for SF cvs import not to screw up CPAN

--- lib/PDF/API2/Resource/CIDFont/CJKFont/adobemingstdlightacro.pm
+++ lib/PDF/API2/Resource/CIDFont/CJKFont/adobemingstdlightacro.pm
@@ -17671,6 +17671,9 @@ __END__
 =head1 HISTORY
 
     $Log$
+    Revision 2.0  2005/11/16 02:16:04  areibens
+    revision workaround for SF cvs import not to screw up CPAN
+
     Revision 1.2  2005/11/16 01:27:48  areibens
     genesis2
 
