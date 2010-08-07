59f41f111841ca0d6edc747233f16dcaf26ba8e2
Steve Simms <steve@deefs.net>
2010-08-06 22:52:33 -0400

    Refactor PDF::API2::Content::Text

--- lib/PDF/API2/Content/Text.pm
+++ lib/PDF/API2/Content/Text.pm
@@ -1,26 +1,17 @@
 package PDF::API2::Content::Text;
 
-BEGIN {
+use warnings;
+use strict;
 
-    use strict;
-    use vars qw(@ISA $VERSION);
+use base 'PDF::API2::Content';
 
-    use PDF::API2::Util;
-    use PDF::API2::Basic::PDF::Utils;
-    use PDF::API2::Content;
-
-    @ISA = qw(PDF::API2::Content);
-
-    ( $VERSION ) = '2.000';
-
-}
-no warnings qw[ deprecated recursion uninitialized ];
+our $VERSION = '2.000';
 
 sub new {
-  my ($class)=@_;
-  my $self = $class->SUPER::new(@_);
-  $self->textstart;
-  return($self);
+    my ($class) = @_;
+    my $self = $class->SUPER::new(@_);
+    $self->textstart();
+    return $self;
 }
 
 1;
