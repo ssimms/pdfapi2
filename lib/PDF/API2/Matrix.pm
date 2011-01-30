25923ccc69d0ced7a441e5ac53ee6dc1f1007330
Steve Simms <ssimms@users.noreply.github.com>
2011-01-30 18:29:04 -0500

    Remove unused functions from Matrix.  The rest are duplicated by Math::Matrix, so that could be a drop-in replacement later on.

--- lib/PDF/API2/Matrix.pm
+++ lib/PDF/API2/Matrix.pm
@@ -23,18 +23,6 @@ sub new {
     bless $self, $type;
 }
 
-sub concat {
-    my $self = shift;
-    my $other = shift;
-    my $result = new PDF::API2::Matrix (@{$self});
-
-    return undef if scalar(@{$self}) != scalar(@{$other});
-    for my $i (0 .. $#{$self}) {
-    push @{$result->[$i]}, @{$other->[$i]};
-    }
-    $result;
-}
-
 sub transpose {
     my $self = shift;
     my @result;
@@ -71,65 +59,12 @@ sub multiply {
     return undef if $#{$self->[0]} != $#{$other->[0]};
     for my $row (@{$self}) {
         my $rescol = [];
-    for my $col (@{$other}) {
+        for my $col (@{$other}) {
             push(@{$rescol}, vekpro($row,$col));
         }
         push(@result, $rescol);
     }
     new PDF::API2::Matrix (@result);
-}
-
-
-sub solve {
-    my $m    = new PDF::API2::Matrix (@{$_[0]});
-    my $mr   = $#{$m};
-    my $mc   = $#{$m->[0]};
-    my $f;
-    my $try;
-    my $k;
-    my $i;
-    my $j;
-    my $eps = 0.000001;
-
-    return undef if $mc <= $mr;
-    ROW: for($i = 0; $i <= $mr; $i++) {
-    $try=$i;
-    # make diagonal element nonzero if possible
-    while (abs($m->[$i]->[$i]) < $eps) {
-        last ROW if $try++ > $mr;
-        my $row = splice(@{$m},$i,1);
-        push(@{$m}, $row);
-    }
-
-    # normalize row
-    $f = $m->[$i]->[$i];
-    for($k = 0; $k <= $mc; $k++) {
-            $m->[$i]->[$k] /= $f;
-    }
-    # subtract multiple of designated row from other rows
-        for($j = 0; $j <= $mr; $j++) {
-        next if $i == $j;
-            $f = $m->[$j]->[$i];
-            for($k = 0; $k <= $mc; $k++) {
-                $m->[$j]->[$k] -= $m->[$i]->[$k] * $f;
-            }
-        }
-    }
-# Answer is in augmented column
-    transpose new PDF::API2::Matrix @{$m->transpose}[$mr+1 .. $mc];
-}
-
-sub print {
-    my $self = shift;
-
-    print STDERR "Matrix: \n";
-    print @_ if scalar(@_);
-    for my $row (@{$self}) {
-        for my $col (@{$row}) {
-            printf STDERR "%10.5f ", $col;
-        }
-        print STDERR "\n";
-    }
 }
 
 1;
