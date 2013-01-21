15d5354b75220ec77cb83ac31faf77518ee9256a
Steve Simms <steve@deefs.net>
2013-01-21 01:40:29 -0500

    Rewrite the RunLengthDecode filter so that it actually works.
    Edge cases aren't being tested yet.  A quick look at a Devel::Cover
    report using the new filter-runlengthdecode test file will show which
    areas need tests.

--- lib/PDF/API2/Basic/PDF/Filter/RunLengthDecode.pm
+++ lib/PDF/API2/Basic/PDF/Filter/RunLengthDecode.pm
@@ -5,87 +5,104 @@ package PDF::API2::Basic::PDF::Filter::RunLengthDecode;
 use base 'PDF::API2::Basic::PDF::Filter';
 
 use strict;
-no warnings qw[ deprecated recursion uninitialized ];
+use warnings;
 
-=head1 NAME
+# Maintainer's Note: RunLengthDecode is described in the PDF 1.7 spec
+# in section 7.4.5.
 
-PDF::API2::Basic::PDF::RunLengthDecode - Run Length encoding filter for PDF streams. Inherits from
-L<PDF::API2::Basic::PDF::Filter>
+sub outfilt {
+    my ($self, $input, $include_eod) = @_;
+    my $output;
 
-=cut
+    while ($input ne '') {
+        my ($unrepeated, $repeated);
 
-sub outfilt
-{
-    my ($self, $str, $isend) = @_;
-    my ($res, $s, $r);
-
-# no state information, just slight inefficiency at block boundaries
-    while ($str ne "")
-    {
-        if ($str =~ m/^(.*?)((.)\2{2,127})(.*?)$/so)
-        {
-            $s = $1;
-            $r = $2;
-            $str = $3;
-        } else
-        {
-            $s = $str;
-            $r = '';
-            $str = '';
+        # Look for a repeated character (which can be repeated up to
+        # 127 times)
+        if ($input =~ m/^(.*?)((.)\3{1,127})(.*)$/so) {
+            $unrepeated = $1;
+            $repeated = $2;
+            $input = $4;
         }
-        while (length($s) > 127)
-        {
-            $res .= pack("C", 127) . substr($s, 0, 127);
-            substr($s, 0, 127) = '';
+        else {
+            $unrepeated = $input;
         }
-        $res .= pack("C", length($s)) . $s if length($s) > 0;
-        $res .= pack("C", 257 - length($r));
+
+        # Print any non-repeating bytes at the beginning of the input
+        # in chunks of up to 128 bytes, prefixed with a run-length (0
+        # to 127, signifying 1 to 128 bytes)
+        while (length($unrepeated) > 127) {
+            $output .= pack('C', 127) . substr($unrepeated, 0, 128);
+            substr($unrepeated, 0, 128) = '';
+        }
+        $output .= pack('C', length($unrepeated) - 1) . $unrepeated if length($unrepeated) > 0;
+
+        # Then print the number of times the repeated byte was
+        # repeated (using the formula "257 - length" to give a result
+        # in the 129-255 range) followed by the byte to be repeated
+        if (length($repeated)) {
+            $output .= pack('C', 257 - length($repeated)) . substr($repeated, 0, 1);
+        }
     }
-    $res .= "\x80" if ($isend);
-    $res;
+
+    # A byte value of 128 signifies that we're done.
+    $output .= "\x80" if $include_eod;
+
+    return $output;
 }
 
-sub infilt
-{
-    my ($self, $str, $isend) = @_;
-    my ($res, $l, $d);
+sub infilt {
+    my ($self, $input, $is_terminated) = @_;
+    my ($output, $length);
 
-    if ($self->{'incache'} ne "")
-    {
-        $str = $self->{'incache'} . $str;
-        $self->{'incache'} = "";
+    # infilt may be called multiple times, and is expected to continue
+    # where it left off
+    if (exists $self->{'incache'}) {
+        $input = $self->{'incache'} . $input;
+        delete $self->{'incache'};
     }
-    while ($str ne "")
-    {
-        $l = unpack("C", $str);
-        if ($l == 128)
-        {
-            $isend = 1;
-            return $res;
+
+    while (length($input)) {
+        # Read a length byte
+        $length = unpack("C", $input);
+
+        # A "length" of 128 represents the end of the document
+        if ($length == 128) {
+            return $output;
         }
-        if ($l > 128)
-        {
-            if (length($str) < 2)
-            {
-                warn "Premature end to data in RunLengthEncoded data" if $isend;
-                $self->{'incache'} = $str;
-                return $res;
+
+        # Any other length needs to be followed by at least one other byte
+        if (length($input) == 1 and not $is_terminated) {
+            die "Premature end to RunLengthEncoded data";
+        }
+
+        # A length of 129-255 represents a repeated string
+        # (number of repeats = 257 - length)
+        if ($length > 128) {
+            if (length($input) == 1) {
+                # Out of data.  Defer until the next call.
+                $self->{'incache'} = $input;
+                return $output;
             }
-            $res .= substr($str, 1, 1) x (257 - $l);
-            substr($str, 0, 2) = "";
-        } else
-        {
-            if (length($str) < $l + 1)
-            {
-                warn "Premature end to data in RunLengthEncoded data" if $isend;
-                $self->{'incache'} = $str;
-                return $res;
+            $output .= substr($input, 1, 1) x (257 - $length);
+            substr($input, 0, 2) = '';
+        }
+
+        # Any other length (under 128) represents a non-repeated
+        # stream of bytes (with a length of 0 to 127 representing 1 to
+        # 128 bytes)
+        else {
+            if (length($input) < $length + 2) {
+                # Insufficient data.  Defer until the next call.
+                $self->{'incache'} = $input;
+                return $output;
             }
-            $res .= substr($str, 1, $l);
-            substr($str, 0, $l + 1) = "";
+            $output .= substr($input, 1, $length + 1);
+            substr($input, 0, $length + 2) = '';
         }
     }
-    $res;
+
+    return $output;
 }
 
 1;
