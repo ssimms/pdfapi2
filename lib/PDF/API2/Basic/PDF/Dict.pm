de1727c2a8c8a0dcefe6ddbbdcdc4357cb72f00b
Steve Simms <ssimms@users.noreply.github.com>
2013-01-25 23:29:14 -0500

    Update coding style.

--- lib/PDF/API2/Basic/PDF/Dict.pm
+++ lib/PDF/API2/Basic/PDF/Dict.pm
@@ -21,13 +21,11 @@ no warnings qw[ deprecated recursion uninitialized ];
 
 our $mincache;
 our $tempbase;
-our $cr = '(?:\015|\012|(?:\015\012))';
 
 use PDF::API2::Basic::PDF::Filter;
 use PDF::API2::Basic::PDF::Name;
 
-BEGIN
-{
+BEGIN {
     my $temp_dir = -d '/tmp' ? '/tmp' : $ENV{TMP} || $ENV{TEMP};
     $tempbase = sprintf("%s/%d-%d-0000", $temp_dir, $$, time());
     $mincache = 32768;
@@ -64,17 +62,14 @@ source PDF the stream starts.
 
 =cut
 
-sub new
-{
-    my ($class, @opts) = @_;
-    my ($self);
+sub new {
+    my ($class) = @_;
+    $class = ref($class) if ref($class);
 
-    $class = ref $class if ref $class;
-    $self = $class->SUPER::new(@_);
+    my $self = $class->SUPER::new(@_);
     $self->{' realised'} = 1;
     return $self;
 }
-
 
 =head2 $d->outobjdeep($fh)
 
@@ -86,140 +81,117 @@ stream's dictionary.
 
 =cut
 
-sub outobjdeep
-{
+sub outobjdeep {
     my ($self, $fh, $pdf, %opts) = @_;
-    my ($key, $val, $f, @filts);
-    my ($loc, $str, %specs, $len);
 
-    if (defined $self->{' stream'} or defined $self->{' streamfile'} or defined $self->{' streamloc'})
-    {
-        if($self->{'Filter'} && $self->{' nofilt'})
-        {
-            $self->{Length}||= PDF::API2::Basic::PDF::Number->new(length($self->{' stream'}));
+    if (defined $self->{' stream'} or defined $self->{' streamfile'} or defined $self->{' streamloc'}) {
+        if ($self->{'Filter'} and $self->{' nofilt'}) {
+            $self->{'Length'} ||= PDF::API2::Basic::PDF::Number->new(length($self->{' stream'}));
         }
-        elsif($self->{'Filter'} || !defined $self->{' stream'})
-        {
-            $self->{'Length'} = PDF::API2::Basic::PDF::Number->new(0) unless (defined $self->{'Length'});
-            $pdf->new_obj($self->{'Length'}) unless ($self->{'Length'}->is_obj($pdf));
+        elsif ($self->{'Filter'} or not defined $self->{' stream'}) {
+            $self->{'Length'} = PDF::API2::Basic::PDF::Number->new(0) unless defined $self->{'Length'};
+            $pdf->new_obj($self->{'Length'}) unless $self->{'Length'}->is_obj($pdf);
         } 
-        else 
-        {
+        else {
             $self->{'Length'} = PDF::API2::Basic::PDF::Number->new(length($self->{' stream'}));
             ## $self->{'Length'} = PDF::API2::Basic::PDF::Number->new(length($self->{' stream'}) + 1);
             ## this old code seams to burp acro6, lets see what breaks next -- fredo
         }
     }
 
-    $fh->print("<< ");
-    foreach ('Type', 'Subtype')
-    {
-        $specs{$_} = 1;
-        if (defined $self->{$_})
-        {
-            $fh->print('/'.PDF::API2::Basic::PDF::Name::string_to_name($_).' ');
-            $self->{$_}->outobj($fh, $pdf, %opts);
-            $fh->print(" ");
-        }
-    }
-    foreach $key (sort keys %$self) {
-        $val = $self->{$key};
-        next if ($key =~ m/^[\s\-]/o || $specs{$key});
-        next if (($val || '') eq '');
-        $key = PDF::API2::Basic::PDF::Name::string_to_name ($key, $pdf);
-        $fh->print("/$key ");
-        $val->outobj($fh, $pdf, %opts);
-        $fh->print(" ");
+    $fh->print('<< ');
+    foreach my $key (sort {
+                         $a eq 'Type'    ? -1 : $b eq 'Type'    ? 1 :
+                         $a eq 'Subtype' ? -1 : $b eq 'Subtype' ? 1 : $a cmp $b
+                     } keys %$self) {
+        next if $key =~ m/^[\s\-]/o;
+        next unless $self->{$key};
+        $fh->print('/' . PDF::API2::Basic::PDF::Name::string_to_name($key, $pdf) . ' ');
+        $self->{$key}->outobj($fh, $pdf, %opts);
+        $fh->print(' ');
     }
     $fh->print('>>');
 
-    #now handle the stream (if any)
-    if (defined $self->{' streamloc'} && !defined $self->{' stream'})
-    {                                   # read a stream if infile
-        $loc = $fh->tell;
-        $self->read_stream;
+    # Now handle the stream (if any)
+    my (@filters, $loc);
+
+    if (defined $self->{' streamloc'} and not defined $self->{' stream'}) {
+        # read a stream if in file
+        $loc = $fh->tell();
+        $self->read_stream();
         $fh->seek($loc, 0);
     }
 
-    if (!$self->{' nofilt'}
-            && (defined $self->{' stream'} || defined $self->{' streamfile'})
-            && defined $self->{'Filter'})
-    {
-        my ($hasflate) = -1;
-        my ($temp, $i, $temp1);
-
-        for ($i = 0; $i < scalar @{$self->{'Filter'}{' val'}}; $i++)
-        {
-            $temp = $self->{'Filter'}{' val'}[$i]->val;
-            if ($temp eq 'LZWDecode')               # hack to get around LZW patent
-            {
-                if ($hasflate < -1)
-                {
+    if (not $self->{' nofilt'} and defined $self->{'Filter'} and (defined $self->{' stream'} or defined $self->{' streamfile'})) {
+        my $hasflate = -1;
+        for my $i (0 .. scalar(@{$self->{'Filter'}{' val'}}) - 1) {
+            my $filter = $self->{'Filter'}{' val'}[$i]->val();
+            # hack to get around LZW patent
+            if ($filter eq 'LZWDecode') {
+                if ($hasflate < -1) {
                     $hasflate = $i;
                     next;
                 }
-                $temp = 'FlateDecode';
-                $self->{'Filter'}{' val'}[$i]{'val'} = $temp;      # !!!
-            } elsif ($temp eq 'FlateDecode')
-            { $hasflate = -2; }
-            $temp1 = "PDF::API2::Basic::PDF::Filter::$temp";
-            push (@filts, $temp1->new);
+                $filter = 'FlateDecode';
+                $self->{'Filter'}{' val'}[$i]{'val'} = $filter;      # !!!
+            }
+            elsif ($filter eq 'FlateDecode') {
+                $hasflate = -2;
+            }
+            my $filter_class = "PDF::API2::Basic::PDF::Filter::$filter";
+            push (@filters, $filter_class->new());
         }
-        splice(@{$self->{'Filter'}{' val'}}, $hasflate, 1) if ($hasflate > -1);
+        splice(@{$self->{'Filter'}{' val'}}, $hasflate, 1) if $hasflate > -1;
     }
 
     if (defined $self->{' stream'}) {
-
         $fh->print(" stream\n");
-        $loc = $fh->tell;
-        $str = $self->{' stream'};
-        unless ($self->{' nofilt'})
-        {
-            foreach $f (reverse @filts)
-            { $str = $f->outfilt($str, 1); }
+        $loc = $fh->tell();
+        my $stream = $self->{' stream'};
+        unless ($self->{' nofilt'}) {
+            foreach my $filter (reverse @filters) {
+                $stream = $filter->outfilt($stream, 1);
+            }
         }
-        $fh->print($str);
+        $fh->print($stream);
         ## $fh->print("\n"); # newline goes into endstream
 
-    } elsif (defined $self->{' streamfile'}) {
-
+    }
+    elsif (defined $self->{' streamfile'}) {
         open(DICTFH, $self->{' streamfile'}) || die "Unable to open $self->{' streamfile'}";
-        binmode(DICTFH,':raw');
+        binmode(DICTFH, ':raw');
 
         $fh->print(" stream\n");
-        $loc = $fh->tell;
-        while (read(DICTFH, $str, 4096))
-        {
-            unless ($self->{' nofilt'})
-            {
-                foreach $f (reverse @filts)
-                { $str = $f->outfilt($str, 0); }
+        $loc = $fh->tell();
+        my $stream;
+        while (read(DICTFH, $stream, 4096)) {
+            unless ($self->{' nofilt'}) {
+                foreach my $filter (reverse @filters) {
+                    $stream = $filter->outfilt($stream, 0);
+                }
             }
-            $fh->print($str);
+            $fh->print($stream);
         }
-        close(DICTFH);
-        unless ($self->{' nofilt'})
-        {
-            $str = '';
-            foreach $f (reverse @filts)
-            { $str = $f->outfilt($str, 1); }
-            $fh->print($str);
+        close DICTFH;
+        unless ($self->{' nofilt'}) {
+            $stream = '';
+            foreach my $filter (reverse @filters) {
+                $stream = $filter->outfilt($stream, 1);
+            }
+            $fh->print($stream);
         }
         ## $fh->print("\n"); # newline goes into endstream
     }
 
-    if (defined $self->{' stream'} or defined $self->{' streamfile'})
-    {
-        $len = $fh->tell - $loc;
-        if ($self->{'Length'}{'val'} != $len)
-        {
-            $self->{'Length'}{'val'} = $len;
-            $pdf->out_obj($self->{'Length'}) if ($self->{'Length'}->is_obj($pdf));
+    if (defined $self->{' stream'} or defined $self->{' streamfile'}) {
+        my $length = $fh->tell() - $loc;
+        unless ($self->{'Length'}{'val'} == $length) {
+            $self->{'Length'}{'val'} = $length;
+            $pdf->out_obj($self->{'Length'}) if $self->{'Length'}->is_obj($pdf);
         }
 
         $fh->print("\nendstream"); # next is endobj which has the final cr
     }
-
 }
 
 =head2 $d->read_stream($force_memory)
@@ -233,61 +205,65 @@ variable.
 
 =cut
 
-sub read_stream
-{
+sub read_stream {
     my ($self, $force_memory) = @_;
-    my ($fh) = $self->{' streamsrc'};
-    my (@filts, $f, $last, $i, $dat);
-    my ($len) = $self->{'Length'}->val;
+
+    my $fh = $self->{' streamsrc'};
+    my $len = $self->{'Length'}->val();
 
     $self->{' stream'} = '';
 
-    if (defined $self->{'Filter'})
-    {
-        foreach $f ($self->{'Filter'}->elementsof)
-        {
-            my ($temp) = "PDF::API2::Basic::PDF::Filter::" . $f->val;
-            push(@filts, $temp->new());
+    my @filters;
+    if (defined $self->{'Filter'}) {
+        foreach my $filter ($self->{'Filter'}->elementsof()) {
+            my $filter_class = "PDF::API2::Basic::PDF::Filter::" . $filter->val();
+            push(@filters, $filter_class->new());
         }
     }
 
-    $last = 0;
-    if (defined $self->{' streamfile'})
-    {
+    my $last = 0;
+    if (defined $self->{' streamfile'}) {
         unlink ($self->{' streamfile'});
         $self->{' streamfile'} = undef;
     }
-    seek ($fh, $self->{' streamloc'}, 0);
-    for ($i = 0; $i < $len; $i += 4096)
-    {
-        if ($i + 4096 > $len)
-        {
-            $last = 1;
-            read($fh, $dat, $len - $i);
+    seek $fh, $self->{' streamloc'}, 0;
+    my ($i, $data);
+    for ($i = 0; $i < $len; $i += 4096) {
+        unless ($i + 4096 > $len) {
+            read $fh, $data, 4096;
         }
-        else
-        { read($fh, $dat, 4096); }
+        else {
+            $last = 1;
+            read $fh, $data, $len - $i;
+        }
 
-        foreach $f (@filts)
-        { $dat = $f->infilt($dat, $last); }
-        if (!$force_memory && !defined $self->{' streamfile'} && ((length($dat) * 2) > $mincache))
-        {
-            open (DICTFH, ">$tempbase") || next;
-            binmode(DICTFH,':raw');
+        foreach my $filter (@filters) {
+            $data = $filter->infilt($data, $last);
+        }
+
+        # Maintainer's Note: There are a couple of issues here:
+        # 1) File::Temp should be used for creating temporary files
+        # 2) The length check should be looking at $self->{' stream'}
+        #    rather than $data (which just contains the latest chunk)
+        if (not $force_memory and not defined $self->{' streamfile'} and ((length($data) * 2) > $mincache)) {
+            open(DICTFH, '>', $tempbase) or next;
+            binmode DICTFH, ':raw';
             $self->{' streamfile'} = $tempbase;
-            $tempbase =~ s/-(\d+)$/"-" . ($1 + 1)/oe;        # prepare for next use
+            $tempbase =~ s/-(\d+)$/'-' . ($1 + 1)/oe;        # prepare for next use
             print DICTFH $self->{' stream'};
             undef $self->{' stream'};
         }
-        if (defined $self->{' streamfile'})
-        { print DICTFH $dat; }
-        else
-        { $self->{' stream'} .= $dat; }
+        if (defined $self->{' streamfile'}) {
+            print DICTFH $data;
+        }
+        else {
+            $self->{' stream'} .= $data;
+        }
     }
 
-    close DICTFH if (defined $self->{' streamfile'});
+    close DICTFH if defined $self->{' streamfile'};
     $self->{' nofilt'} = 0;
-    $self;
+    return $self;
 }
 
 =head2 $d->val
@@ -296,7 +272,8 @@ Returns the dictionary, which is itself.
 
 =cut
 
-sub val
-{ $_[0]; }
+sub val {
+    return $_[0];
+}
 
 1;
