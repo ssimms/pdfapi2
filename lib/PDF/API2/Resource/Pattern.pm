963c1625aa677622c9205b268102277658a01aa4
Steve Simms <ssimms@users.noreply.github.com>
2014-06-13 16:53:54 -0400

    Update coding style, remove duplicated code from base class.

--- lib/PDF/API2/Resource/Pattern.pm
+++ lib/PDF/API2/Resource/Pattern.pm
@@ -4,39 +4,19 @@ package PDF::API2::Resource::Pattern;
 
 use base 'PDF::API2::Resource';
 
-use PDF::API2::Basic::PDF::Utils;
-use PDF::API2::Util;
-
-no warnings qw[ deprecated recursion uninitialized ];
+use strict;
+use warnings;
 
 sub new {
-    my ($class,$pdf,$key,%opts)=@_;
+    my ($class, $pdf, $name) = @_;
+    my $self = $class->SUPER::new($pdf, $name);
 
-    $class = ref $class if ref $class;
-    $self=$class->SUPER::new($pdf,$key || pdfkey());
-    $pdf->new_obj($self) unless($self->is_obj($pdf));
-    $self->{Type}=PDFName('Pattern');
-    $self->{' apipdf'}=$pdf;
+    $self->type('Pattern');
 
-    return($self);
+    return $self;
 }
 
-sub new_api {
-    my ($class,$api,@opts)=@_;
-
-    my $obj=$class->new($api->{pdf},@opts);
-    $self->{' api'}=$api;
-
-    return($obj);
-}
-
-sub outobjdeep {
-    my ($self, @opts) = @_;
-    foreach my $k (qw/ api apipdf /) {
-        $self->{" $k"}=undef;
-        delete($self->{" $k"});
-    }
-    $self->SUPER::outobjdeep(@opts);
-}
+# Deprecated (rolled into new)
+sub new_api { return new(@_); }
 
 1;
