2eb2e91664d5c5d88ee017f1d298081908afff22
Steve Simms <ssimms@users.noreply.github.com>
2014-06-13 22:24:48 -0400

    Update coding style.

--- lib/PDF/API2/Resource/XObject/Form/Hybrid.pm
+++ lib/PDF/API2/Resource/XObject/Form/Hybrid.pm
@@ -4,68 +4,58 @@ package PDF::API2::Resource::XObject::Form::Hybrid;
 
 use base qw(PDF::API2::Content PDF::API2::Resource::XObject::Form);
 
+use strict;
+use warnings;
+
 use PDF::API2::Basic::PDF::Dict;
 use PDF::API2::Basic::PDF::Utils;
 use PDF::API2::Resource::XObject::Form;
-use PDF::API2::Util;
-
-no warnings qw[ deprecated recursion uninitialized ];
 
 sub new {
-    my ($class,$pdf) = @_;
-    my $self;
+    my $self = PDF::API2::Resource::XObject::Form::new(@_);
 
-    $class = ref $class if ref $class;
+    $self->{' stream'} = '';
+    $self->{' poststream'} = '';
+    $self->{' font'} = undef;
+    $self->{' fontsize'} = 0;
+    $self->{' charspace'} = 0;
+    $self->{' hspace'} = 100;
+    $self->{' wordspace'} = 0;
+    $self->{' lead'} = 0;
+    $self->{' rise'} = 0;
+    $self->{' render'} = 0;
+    $self->{' matrix'} = [1, 0, 0, 1, 0, 0];
+    $self->{' fillcolor'} = [0];
+    $self->{' strokecolor'} = [0];
+    $self->{' translate'} = [0, 0];
+    $self->{' scale'} = [1, 1];
+    $self->{' skew'} = [0, 0];
+    $self->{' rotate'} = 0;
+    $self->{' apiistext'} = 0;
 
-    $self=PDF::API2::Resource::XObject::Form::new($class,$pdf,pdfkey());
-    $pdf->new_obj($self) unless($self->is_obj($pdf));
+    $self->{'Resources'} = PDFDict();
+    $self->{'Resources'}->{'ProcSet'} = PDFArray(map { PDFName($_) } qw(PDF Text ImageB ImageC ImageI));
 
-    $self->{' apipdf'}=$pdf;
+    $self->compressFlate();
 
-    $self->{' stream'}='';
-    $self->{' poststream'}='';
-    $self->{' font'}=undef;
-    $self->{' fontsize'}=0;
-    $self->{' charspace'}=0;
-    $self->{' hspace'}=100;
-    $self->{' wordspace'}=0;
-    $self->{' lead'}=0;
-    $self->{' rise'}=0;
-    $self->{' render'}=0;
-    $self->{' matrix'}=[1,0,0,1,0,0];
-    $self->{' fillcolor'}=[0];
-    $self->{' strokecolor'}=[0];
-    $self->{' translate'}=[0,0];
-    $self->{' scale'}=[1,1];
-    $self->{' skew'}=[0,0];
-    $self->{' rotate'}=0;
-    $self->{' apiistext'}=0;
-
-    $self->{Resources}=PDFDict();
-    $self->{Resources}->{ProcSet}=PDFArray(map { PDFName($_) } qw[ PDF Text ImageB ImageC ImageI ]);
-
-    $self->compressFlate;
-
-    return($self);
+    return $self;
 }
 
-sub new_api {
-    my ($class,$api,@opts)=@_;
-
-    my $obj=$class->new($api->{pdf},@opts);
-    $obj->{' api'}=$api;
-
-    return($obj);
-}
+# Deprecated (rolled into new)
+sub new_api { return new(@_); }
 
 sub outobjdeep {
-    my ($self, @opts) = @_;
-    $self->textend unless($self->{' nofilt'});
-    foreach my $k (qw/ api apipdf apipage font fontsize charspace hspace wordspace lead rise render matrix fillcolor strokecolor translate scale skew rotate /) {
-        $self->{" $k"}=undef;
-        delete($self->{" $k"});
+    my ($self, @options) = @_;
+    $self->textend() unless $self->{' nofilt'};
+
+    # Maintainer's Note: This list of keys isn't the same as the list
+    # in new().  Should it be?
+    foreach my $key (qw(api apipdf apipage font fontsize charspace hspace
+                        wordspace lead rise render matrix fillcolor
+                        strokecolor translate scale skew rotate)) {
+        delete $self->{" $key"};
     }
-    PDF::API2::Basic::PDF::Dict::outobjdeep($self,@opts);
+    return PDF::API2::Basic::PDF::Dict::outobjdeep($self, @options);
 }
 
 1;
