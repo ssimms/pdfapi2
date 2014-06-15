165d997d327ad6b6d357348d57277d2443b87375
Steve Simms <ssimms@users.noreply.github.com>
2014-06-14 23:34:17 -0400

    Update coding style; add comments; prepend a zero instead of appending when an odd number of digits are encoded

--- lib/PDF/API2/Resource/XObject/Form/BarCode/int2of5.pm
+++ lib/PDF/API2/Resource/XObject/Form/BarCode/int2of5.pm
@@ -4,62 +4,59 @@ package PDF::API2::Resource::XObject::Form::BarCode::int2of5;
 
 use base 'PDF::API2::Resource::XObject::Form::BarCode';
 
-no warnings qw[ deprecated recursion uninitialized ];
+use strict;
+use warnings;
+
+# Interleaved 2 of 5 Barcodes
+
+# Pairs of digits are encoded; the first digit is represented by five
+# bars, and the second digit is represented by five spaces interleaved
+# with the bars.
 
 sub new {
-    my ($class,$pdf,%opts) = @_;
-    my $self;
+    my ($class, $pdf, %options) = @_;
+    my $self = $class->SUPER::new($pdf,%options);
 
-    $class = ref $class if ref $class;
+    my @bars = $self->encode($options{'-code'});
 
-    $self=$class->SUPER::new($pdf,%opts);
+    $self->drawbar([@bars]);
 
-    my @bar = $self->encode($opts{-code});
-
-    $self->drawbar([@bar]);
-
-    return($self);
+    return $self;
 }
 
-
-my @bar25interleaved=qw(
-    11221
-    21112
-    12112
-    22111
-    11212
-    21211
-    12211
-    11122
-    21121
-    12121
-);
+my @bar25interleaved = qw(11221 21112 12112 22111 11212 21211 12211 11122 21121 12121);
 
 sub encode {
-    my $self=shift @_;
-    my $string=shift @_;
-    $string=~ tr/0123456789//cd;
-    my ($enc,@bar);
+    my ($self, $string) = @_;
 
-    push(@bar,'aaaa');
-    while(length($string)>0)
-    {
-        $string=~ s/^(\d{1,1})(\d{0,1})(\d*?)$/$3/;
-        $c1=$1;
-        $c2=$2;
-        $c2='0' if ($c2 eq "");
-        $s1=$bar25interleaved[$c1];
-        $s2=$bar25interleaved[$c2];
-        $o='';
-        for($cnt=0;$cnt<5;$cnt++)
-        {
-            $o.=substr($s1,$cnt,1);
-            $o.=substr($s2,$cnt,1);
+    # Remove any character that isn't a digit
+    $string =~ s/[^0-9]//g;
+
+    # Prepend a 0 if there is an odd number of digits
+    $string = '0' . $string if length($string) % 2;
+
+    # Start Code
+    my @bars = ('aaaa');
+
+    # Encode pairs of digits
+    my ($c1, $c2, $s1, $s2, $pair);
+    while (length($string)) {
+        ($c1, $c2, $string) = split //, $string, 3;
+
+        $s1 = $bar25interleaved[$c1];
+        $s2 = $bar25interleaved[$c2];
+        $pair = '';
+        foreach my $i (0 .. 4) {
+            $pair .= substr($s1, $i, 1);
+            $pair .= substr($s2, $i, 1);
         }
-        push(@bar,[$o,($c1 . $c2)]);
+        push @bars, [$pair, ($c1 . $c2)];
     }
-    push(@bar,'baaa');
-    return(@bar);
+
+    # Stop Code
+    push @bars, 'baaa';
+
+    return @bars;
 }
 
 1;
