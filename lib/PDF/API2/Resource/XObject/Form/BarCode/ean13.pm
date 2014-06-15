7806f1430f25f8a73287373be7a12596bc41a295
Steve Simms <steve@deefs.net>
2014-06-15 10:39:50 -0400

    Consolidate the code for digits 2-7

--- lib/PDF/API2/Resource/XObject/Form/BarCode/ean13.pm
+++ lib/PDF/API2/Resource/XObject/Form/BarCode/ean13.pm
@@ -18,120 +18,43 @@ sub new {
     return $self;
 }
 
-my @ean_code_odd  = qw( 3211 2221 2122 1411 1132 1231 1114 1312 1213 3112 );
-my @ean_code_even = qw( 1123 1222 2212 1141 2311 1321 4111 2131 3121 2113 );
+my @ean_code_odd  = qw(3211 2221 2122 1411 1132 1231 1114 1312 1213 3112);
+my @ean_code_even = qw(1123 1222 2212 1141 2311 1321 4111 2131 3121 2113);
+my @parity = qw(OOOOOO OOEOEE OOEEOE OOEEEO OEOOEE OEEOOE OEEEOO OEOEOE OEOEEO OEEOEO);
 
 sub encode {
     my ($self, $string) = @_;
-    my @c = split //, $string;
+    my @digits = split //, $string;
 
     # The first digit determines the even/odd pattern of the next six
     # digits, and is printed to the left of the barcode
-    my $v = shift @c;
-    my @bars = (['07', $v]);
+    my $first = shift @digits;
+    my @bars = (['07', $first]);
 
-    # Start Code
+    # Start Guard
     push @bars, 'a1a';
 
     # Digits 2-7
-    if ($v == 0) {
-        foreach (0..5) {
-            my $digit = shift @c;
+    foreach my $i (0 .. 5) {
+        my $digit = shift @digits;
+        if (substr($parity[$first], $i, 1) eq 'O') {
             push @bars, [$ean_code_odd[$digit], $digit];
         }
-    }
-    elsif ($v == 1) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-    }
-    elsif ($v == 2) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-    }
-    elsif ($v == 3) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-    }
-    elsif ($v == 4) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-    }
-    elsif ($v == 5) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-    }
-    elsif ($v == 6) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-    }
-    elsif ($v == 7) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-    }
-    elsif ($v == 8) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-    }
-    elsif ($v == 9) {
-        my $digit = shift @c;
-        push @bars, [$ean_code_odd[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
-        $digit = shift @c; push @bars, [$ean_code_even[$digit], $digit];
-        $digit = shift @c; push @bars, [$ean_code_odd[$digit],  $digit];
+        else {
+            push @bars, [$ean_code_even[$digit], $digit];
+        }
     }
 
-    # Center Code
+    # Center Guard
     push @bars, '1a1a1';
 
     # Digits 8-13
     for (0..5) {
-        my $digit = shift @c;
+        my $digit = shift @digits;
         push @bars, [$ean_code_odd[$digit], $digit];
     }
 
-    # Stop Code
+    # Right Guard
     push @bars, 'a1a';
 
     return @bars;
