18f81bcd903f21a52f152efacd4ed5e4b6151154
Steve Simms <steve@deefs.net>
2016-03-09 09:35:36 -0500

    Code style update.

--- lib/PDF/API2/Basic/PDF/Filter/FlateDecode.pm
+++ lib/PDF/API2/Basic/PDF/Filter/FlateDecode.pm
@@ -78,40 +78,44 @@ sub _depredict_png {
     my $bpp      = ceil($bpc * $comp / 8);
     my $scanline = 1 + ceil($bpp * $columns);
 
-    my $prev='';
-    my $clearstream='';
-    my $lastrow=($height||(length($stream)/$scanline))-1;
-    foreach my $n (0..$lastrow) {
+    my $prev = '';
+    my $clearstream = '';
+    my $lastrow = ($height || (length($stream) / $scanline)) - 1;
+    foreach my $n (0 .. $lastrow) {
         # print STDERR "line $n:";
-        my $line=substr($stream,$n*$scanline,$scanline);
-        my $filter=vec($line,0,8);
-        my $clear='';
-        $line=substr($line,1);
-        # print STDERR " filter=$filter";
-        if($filter==0) {
-            $clear=$line;
-        } elsif($filter==1) {
-            foreach my $x (0..length($line)-1) {
-                vec($clear,$x,8)=(vec($line,$x,8)+vec($clear,$x-$bpp,8))%256;
-            }
-        } elsif($filter==2) {
-            foreach my $x (0..length($line)-1) {
-                vec($clear,$x,8)=(vec($line,$x,8)+vec($prev,$x,8))%256;
-            }
-        } elsif($filter==3) {
-            foreach my $x (0..length($line)-1) {
-                vec($clear,$x,8)=(vec($line,$x,8)+floor((vec($clear,$x-$bpp,8)+vec($prev,$x,8))/2))%256;
-            }
-        } elsif($filter==4) {
-            # die "paeth/png filter not supported.";
-            foreach my $x (0..length($line)-1) {
-                vec($clear,$x,8)=(vec($line,$x,8)+_paeth_predictor(vec($clear,$x-$bpp,8),vec($prev,$x,8),vec($prev,$x-$bpp,8)))%256;
+        my $line = substr($stream, $n * $scanline, $scanline);
+        my $filter = vec($line, 0, 8);
+        my $clear = '';
+        $line = substr($line, 1);
+        # print STDERR " filter=$filter ";
+        if ($filter == 0) {
+            $clear = $line;
+        }
+        elsif ($filter == 1) {
+            foreach my $x (0 .. length($line) - 1) {
+                vec($clear, $x, 8) = (vec($line, $x, 8) + vec($clear, $x - $bpp, 8)) % 256;
             }
         }
-        $prev=$clear;
-        foreach my $x (0..($columns*$comp)-1) {
-            vec($clearstream,($n*$columns*$comp)+$x,$bpc)=vec($clear,$x,$bpc);
-        #    print STDERR "".vec($clear,$x,$bpc).",";
+        elsif ($filter == 2) {
+            foreach my $x (0 .. length($line) - 1) {
+                vec($clear, $x, 8) = (vec($line, $x, 8) + vec($prev, $x, 8)) % 256;
+            }
+        }
+        elsif ($filter == 3) {
+            foreach my $x (0 .. length($line) - 1) {
+                vec($clear, $x, 8) = (vec($line, $x, 8) + floor((vec($clear, $x - $bpp, 8) + vec($prev, $x, 8)) / 2)) % 256;
+            }
+        }
+        elsif ($filter == 4) {
+            # die "paeth/png filter not supported.";
+            foreach my $x (0 .. length($line) - 1) {
+                vec($clear, $x, 8) = (vec($line, $x, 8) + _paeth_predictor(vec($clear, $x - $bpp, 8), vec($prev, $x, 8), vec($prev, $x - $bpp, 8))) % 256;
+            }
+        }
+        $prev = $clear;
+        foreach my $x (0 .. ($columns * $comp) - 1) {
+            vec($clearstream, ($n * $columns * $comp) + $x, $bpc) = vec($clear, $x, $bpc);
+            # print STDERR "" . vec($clear, $x, $bpc) . ",";
         }
         # print STDERR "\n";
     }
@@ -120,16 +124,18 @@ sub _depredict_png {
 }
 
 sub _paeth_predictor {
-    my ($a, $b, $c)=@_;
+    my ($a, $b, $c) = @_;
     my $p = $a + $b - $c;
     my $pa = abs($p - $a);
     my $pb = abs($p - $b);
     my $pc = abs($p - $c);
-    if(($pa <= $pb) && ($pa <= $pc)) {
+    if ($pa <= $pb && $pa <= $pc) {
         return $a;
-    } elsif($pb <= $pc) {
+    }
+    elsif ($pb <= $pc) {
         return $b;
-    } else {
+    }
+    else {
         return $c;
     }
 }
