2ea965a2779fc4118d55016e52a120b15fddb188
Steve Simms <steve@deefs.net>
2019-05-16 21:05:10 -0400

    Update code style

--- lib/PDF/API2/Resource/UniFont.pm
+++ lib/PDF/API2/Resource/UniFont.pm
@@ -1,7 +1,7 @@
 package PDF::API2::Resource::UniFont;
 
 use strict;
-no warnings qw[ deprecated recursion uninitialized ];
+use warnings;
 
 # VERSION
 
@@ -15,7 +15,7 @@ PDF::API2::Resource::UniFont - Unicode Font Support
 
 =over
 
-=item $font = PDF::API2::Resource::UniFont->new $pdf, @fontspecs, %options
+=item $font = PDF::API2::Resource::UniFont->new($pdf, @fontspecs, %options);
 
 Returns a uni-font object.
 
@@ -35,7 +35,7 @@ B<BLOCKSPECS:>
 
     [
         $block1, $block3,    # register font for block 1 + 3
-        [$blockA,$blockZ],   # register font for blocks A .. Z
+        [$blockA, $blockZ],  # register font for blocks A .. Z
     ]
 
 B<CODESPECS:>
@@ -56,214 +56,160 @@ Valid %options are:
 =cut
 
 sub new {
-    my ($class,$pdf,@fonts) = @_;
+    my ($class, $pdf, @fonts) = @_;
 
-    $class = ref $class if ref $class;
-    my $self={
-        fonts=>[],
-        block=>{},
-        code=>{},
+    $class = ref($class) if ref($class);
+    my $self = {
+        fonts => [],
+        block => {},
+        code => {},
     };
-    bless $self,$class;
+    bless $self, $class;
 
-    $self->{pdf}=$pdf;
+    $self->{'pdf'} = $pdf;
 
     # look at all fonts
-    my $fn=0;
-    while (ref $fonts[0])
-    {
-        my $font=shift @fonts;
-        if(ref($font) eq 'ARRAY')
-        {
-            push @{$self->{fonts}},$font->[0];
-            shift @{$font};
-            while(defined $font->[0])
-            {
-                my $r0=shift @{$font};
-                if(ref $r0)
-                {
-                    foreach my $b ($r0->[0]..$r0->[-1])
-                    {
-                        $self->{block}->{$b}=$fn;
+    my $fn = 0;
+    while (ref($fonts[0])) {
+        my $font = shift @fonts;
+        if (ref($font) eq 'ARRAY') {
+            push @{$self->{'fonts'}}, $font->[0];
+            shift @$font;
+            while (defined $font->[0]) {
+                my $r0 = shift @$font;
+                if (ref($r0)) {
+                    foreach my $b ($r0->[0] .. $r0->[-1]) {
+                        $self->{'block'}->{$b} = $fn;
                     }
                 }
-                else
-                {
-                    $self->{block}->{$r0}=$fn;
+                else {
+                    $self->{'block'}->{$r0} = $fn;
                 }
             }
         }
-        elsif(ref($font) eq 'HASH')
-        {
-            push @{$self->{fonts}},$font->{font};
+        elsif (ref($font) eq 'HASH') {
+            push @{$self->{'fonts'}}, $font->{'font'};
 
-            if(defined $font->{blocks} && ref($font->{blocks}) eq 'ARRAY')
-            {
-                foreach my $r0 (@{$font->{blocks}})
-                {
-                    if(ref $r0)
-                    {
-                        foreach my $b ($r0->[0]..$r0->[-1])
-                        {
-                            $self->{block}->{$b}=$fn;
+            if (defined($font->{'blocks'}) and ref($font->{'blocks'}) eq 'ARRAY') {
+                foreach my $r0 (@{$font->{'blocks'}}) {
+                    if (ref($r0)) {
+                        foreach my $b ($r0->[0] .. $r0->[-1]) {
+                            $self->{'block'}->{$b} = $fn;
                         }
                     }
-                    else
-                    {
-                        $self->{block}->{$r0}=$fn;
+                    else {
+                        $self->{'block'}->{$r0} = $fn;
                     }
                 }
             }
 
-            if(defined $font->{codes} && ref($font->{codes}) eq 'ARRAY')
-            {
-                foreach my $r0 (@{$font->{codes}})
-                {
-                    if(ref $r0)
-                    {
-                        foreach my $b ($r0->[0]..$r0->[-1])
-                        {
-                            $self->{code}->{$b}=$fn;
+            if (defined($font->{'codes'}) and ref($font->{'codes'}) eq 'ARRAY') {
+                foreach my $r0 (@{$font->{'codes'}}) {
+                    if (ref($r0)) {
+                        foreach my $b ($r0->[0] .. $r0->[-1]) {
+                            $self->{'code'}->{$b} = $fn;
                         }
                     }
-                    else
-                    {
-                        $self->{code}->{$r0}=$fn;
+                    else {
+                        $self->{'code'}->{$r0} = $fn;
                     }
                 }
             }
         }
-        else
-        {
-            push @{$self->{fonts}},$font;
-            foreach my $b (0..255)
-            {
-                $self->{block}->{$b}=$fn;
+        else {
+            push @{$self->{'fonts'}}, $font;
+            foreach my $b (0 .. 255) {
+                $self->{'block'}->{$b} = $fn;
             }
         }
         $fn++;
     }
 
-    my %opts=@fonts;
+    my %opts = @fonts;
 
-    $self->{encode}=$opts{-encode} if(defined $opts{-encode});
+    $self->{'encode'} = $opts{'-encode'} if defined $opts{'-encode'};
 
-    return($self);
+    return $self;
 }
 
-sub isvirtual { return(1); }
+sub isvirtual { return 1; }
 
-sub fontlist
-{
-    my ($self)=@_;
-    return [@{$self->{fonts}}];
+sub fontlist {
+    my $self = shift();
+    return [@{$self->{'fonts'}}];
 }
 
 sub width {
-    my ($self,$text)=@_;
-    $text=decode($self->{encode},$text) unless(is_utf8($text));
-    my $width=0;
-    if(1)
-    {
-        my @blks=();
-        foreach my $u (unpack('U*',$text))
-        {
-            my $fn=0;
-            if(defined $self->{code}->{$u})
-            {
-                $fn=$self->{code}->{$u};
-            }
-            elsif(defined $self->{block}->{($u>>8)})
-            {
-                $fn=$self->{block}->{($u>>8)};
-            }
-            else
-            {
-                $fn=0;
-            }
-            if(scalar @blks==0 || $blks[-1]->[0]!=$fn)
-            {
-                push @blks,[$fn,pack('U',$u)];
-            }
-            else
-            {
-                $blks[-1]->[1].=pack('U',$u);
-            }
+    my ($self, $text) = @_;
+    $text = decode($self->{'encode'}, $text) unless is_utf8($text);
+    my $width = 0;
+
+    my @blks = ();
+    foreach my $u (unpack('U*', $text)) {
+        my $fn = 0;
+        if (defined $self->{code}->{$u}) {
+            $fn = $self->{'code'}->{$u};
         }
-        foreach my $blk (@blks)
-        {
-            $width+=$self->fontlist->[$blk->[0]]->width($blk->[1]);
+        elsif (defined $self->{'block'}->{$u >> 8}) {
+            $fn = $self->{'block'}->{$u >> 8};
+        }
+        else {
+            $fn = 0;
+        }
+        if (scalar @blks == 0 || $blks[-1]->[0] != $fn) {
+            push @blks, [$fn, pack('U', $u)];
+        }
+        else {
+            $blks[-1]->[1] .= pack('U', $u);
         }
     }
-    else
-    {
-        foreach my $u (unpack('U*',$text))
-        {
-            if(defined $self->{code}->{$u})
-            {
-                $width+=$self->fontlist->[$self->{code}->{$u}]->width(pack('U',$u));
-            }
-            elsif(defined $self->{block}->{($u>>8)})
-            {
-                $width+=$self->fontlist->[$self->{block}->{($u>>8)}]->width(pack('U',$u));
-            }
-            else
-            {
-                $width+=$self->fontlist->[0]->width(pack('U',$u));
-            }
-        }
+    foreach my $blk (@blks) {
+        $width += $self->fontlist->[$blk->[0]]->width($blk->[1]);
     }
 
-    return($width);
+    return $width;
 }
 
-sub text
-{
-    my ($self,$text,$size,$ident)=@_;
-    $text=decode($self->{encode},$text) unless(is_utf8($text));
-    die 'textsize not specified' unless(defined $size);
-    my $newtext='';
-    my $lastfont=-1;
-    my @codes=();
+sub text {
+    my ($self, $text, $size, $indent) = @_;
+    $text = decode($self->{'encode'}, $text) unless is_utf8($text);
+    die 'textsize not specified' unless defined $size;
 
-    foreach my $u (unpack('U*',$text))
-    {
-        my $thisfont=0;
-        if(defined $self->{code}->{$u})
-        {
-            $thisfont=$self->{code}->{$u};
+    my $value = '';
+    my $lastfont = -1;
+    my @codes;
+
+    foreach my $u (unpack('U*', $text)) {
+        my $thisfont = 0;
+        if (defined $self->{'code'}->{$u}) {
+            $thisfont = $self->{'code'}->{$u};
         }
-        elsif(defined $self->{block}->{($u>>8)})
-        {
-            $thisfont=$self->{block}->{($u>>8)};
+        elsif (defined $self->{'block'}->{$u >> 8}) {
+            $thisfont = $self->{'block'}->{$u >> 8};
         }
 
-        if($thisfont!=$lastfont && $lastfont!=-1)
-        {
-            my $f=$self->fontlist->[$lastfont];
-            if(defined($ident) && $ident!=0)
-            {
-	            $newtext.='/'.$f->name.' '.$size.' Tf ['.$ident.' '.$f->text(pack('U*',@codes)).'] TJ ';
-	            $ident=undef;
+        if ($thisfont != $lastfont and $lastfont != -1) {
+            my $font = $self->fontlist->[$lastfont];
+            if (defined($indent) and $indent != 0) {
+                $value .= '/' . $font->name() . ' ' . $size . ' Tf [' . $indent . ' ' . $font->text(pack('U*', @codes)) . '] TJ ';
+                $indent = undef;
             }
-            else
-            {
-	            $newtext.='/'.$f->name.' '.$size.' Tf '.$f->text(pack('U*',@codes)).' Tj ';
+            else {
+                $value .= '/' . $font->name() . ' ' . $size . ' Tf ' . $font->text(pack('U*', @codes)) . ' Tj ';
             }
-            @codes=();
+            @codes = ();
         }
 
-        push(@codes,$u);
-        $lastfont=$thisfont;
+        push @codes, $u;
+        $lastfont = $thisfont;
     }
 
-    if(scalar @codes > 0)
-    {
-        my $f=$self->fontlist->[$lastfont];
-        $newtext.='/'.$f->name.' '.$size.' Tf '.$f->text(pack('U*',@codes),$size).' ';
+    if (scalar @codes > 0) {
+        my $font = $self->fontlist->[$lastfont];
+        $value .= '/' . $font->name() . ' ' . $size . ' Tf ' . $font->text(pack('U*', @codes), $size) . ' ';
     }
 
-    return($newtext);
+    return $value;
 }
 
 =back
