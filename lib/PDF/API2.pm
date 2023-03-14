57f14d24a379b5c5440f4e868595ed8acea6db41
Johan Vromans <jvromans@squirrel.nl>
2023-03-14 19:13:44 +0100

    Fix validating PDF date in CreationDate and ModDate metadata.

--- lib/PDF/API2.pm
+++ lib/PDF/API2.pm
@@ -552,15 +552,17 @@ sub _is_date {
     # D: prefix and the year, all components are optional but must be present if
     # a later component is present.  No provision is made in the specification
     # for leap seconds, etc.
-    return unless $value =~ /^D:([0-9]{4})        # D:YYYY (required)
-                             (?:([01][0-9])       # Month (01-12)
-                             (?:([0123][0-9])     # Day (01-31)
-                             (?:([012][0-9])      # Hour (00-23)
-                             (?:([012345][0-9])   # Minute (00-59)
-                             (?:([012345][0-9])   # Second (00-59)
-                             (?:([Z+-])           # UT Offset Direction
-                             (?:([012][0-9])      # UT Offset Hours
-                             (?:\'([012345][0-9]) # UT Offset Minutes
+    # Note that the 1.7 and ISO specs seem to differ whether a final apostrophe is required.
+    # So accept both forms.
+    return unless $value =~ /^D:([0-9]{4})         # D:YYYY (required)
+                             (?:([01][0-9])        # Month (01-12)
+                             (?:([0123][0-9])      # Day (01-31)
+                             (?:([012][0-9])       # Hour (00-23)
+                             (?:([012345][0-9])    # Minute (00-59)
+                             (?:([012345][0-9])    # Second (00-59)
+                             (?:([Z+-])            # UT Offset Direction
+                             (?:([012][0-9])'      # UT Offset Hours plus apostrophe
+                             (?:([012345][0-9])\'? # UT Offset Minutes plus opt apostrophe
                              )?)?)?)?)?)?)?)?$/x;
     my ($year, $month, $day, $hour, $minute, $second, $od, $oh, $om)
         = ($1, $2, $3, $4, $5, $6, $7, $8, $9);
